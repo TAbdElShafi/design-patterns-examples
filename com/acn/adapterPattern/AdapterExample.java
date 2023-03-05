bea81df47e470f44c3f39e0ef41732682d4d909a
tarek.abdelshafy <Tarek.abdelshafi12@gmail.com>
2023-03-05 15:11:13 +0200

    Implement Adapter structural design pattern which allows incompatible objects to collaborate.

---
+++ com/acn/adapterPattern/AdapterExample.java
@@ -0,0 +1,31 @@
+package com.acn.adapterPattern;
+
+import com.acn.adapterPattern.adapters.SquarePegAdapter;
+import com.acn.adapterPattern.holes.RoundHole;
+import com.acn.adapterPattern.pegs.RoundPeg;
+import com.acn.adapterPattern.pegs.SquarePeg;
+
+public class AdapterExample {
+
+    public static void main(String[] args) {
+        RoundHole hole = new RoundHole(5);
+        RoundPeg roundPeg = new RoundPeg(5);
+        if (hole.fits(roundPeg)) {
+            System.out.println("Round peg r5 fits round hole r5.");
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+
+        // Adapter solves the problem.
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("Square peg w2 fits round hole r5.");
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("Square peg w20 does not fit into round hole r5.");
+        }
+    }
+
+}
---
+++ com/acn/adapterPattern/adapters/SquarePegAdapter.java
@@ -0,0 +1,20 @@
+package com.acn.adapterPattern.adapters;
+
+import com.acn.adapterPattern.pegs.RoundPeg;
+import com.acn.adapterPattern.pegs.SquarePeg;
+
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Calculate a minimum circle radius, which can fit this peg.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
