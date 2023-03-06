cdcd9746b820b371a692c6d30f8e8c4b9fd4d657
tarek.abdelshafy <Tarek.abdelshafi12@gmail.com>
2023-03-06 16:46:26 +0200

    Implement Command behavioral design pattern which converts requests or simple operations into objects.

---
+++ com/acn/commandPattern/CommandExample.java
@@ -0,0 +1,12 @@
+package com.acn.commandPattern;
+
+import com.acn.commandPattern.editor.Editor;
+
+public class CommandExample {
+
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+
+}
---
+++ com/acn/commandPattern/commands/CopyCommand.java
@@ -0,0 +1,16 @@
+package com.acn.commandPattern.commands;
+
+import com.acn.commandPattern.editor.Editor;
+
+public class CopyCommand extends Command {
+
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
